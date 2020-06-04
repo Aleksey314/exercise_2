27e15c8ecc806018d2d8e68f39e48d395d0ada41
Aleksey <ale612@bk.ru>
2020-06-04 21:07:48 +0300

    cryptography

---
+++ EncryptionMessage/EncryptionMessage/Cryptographer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EncryptionMessage
+{
+    class Cryptographer
+    {
+        public byte[] encryptedMessage;
+        public string decryptedMessage;
+        public byte[] encryptedData;
+        public byte[] decryptedData;
+        public byte[] symmetricKeyOut;
+        public byte[] symmetricIVOut;
+        public byte[] symmetricKeyIn;
+        public byte[] symmetricIVIn;
+        public RSAParameters publicKeyRsaIn;
+        public RSAParameters publicKeyRsaOut;
+        private RSAParameters privateKeyRsa;
+        public void EncryptRsa(byte[] dataToEncrypt)
+        {
+            try
+            {
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.ImportParameters(publicKeyRsaIn);
+                    encryptedData = rsa.Encrypt(dataToEncrypt, false);
+                }
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void DecryptRsa(byte[] dataToDecrypt)
+        {
+            try
+            {
+                using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+                {
+                    rsa.ImportParameters(privateKeyRsa);
+                    decryptedData = rsa.Decrypt(dataToDecrypt, false);
+                }
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void CreateRsaKeys()
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                publicKeyRsaOut = rsa.ExportParameters(false);
+                privateKeyRsa = rsa.ExportParameters(true);
+            }
+        }
+        public void CreateAesKeys(string password, string salt)
+        {
+            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, Encoding.Unicode.GetBytes(salt));
+            symmetricKeyOut = rfc2898DeriveBytes.GetBytes(32);
+            symmetricIVOut = rfc2898DeriveBytes.GetBytes(16);
+        }
+        public void EncryptAes(string message)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = symmetricKeyIn;
+                aes.IV = symmetricIVIn;
+                ICryptoTransform cryptoTransform = aes.CreateEncryptor();
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                        {
+                            streamWriter.Write(message);
+                        }
+                        encryptedMessage = memoryStream.ToArray();
+                    }
+                }
+            }
+        }
+        public void DecryptAes(byte[] message)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = symmetricKeyOut;
+                aes.IV = symmetricIVOut;
+                ICryptoTransform cryptoTransform = aes.CreateDecryptor();
+                using (MemoryStream memoryStream = new MemoryStream(message))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(cryptoStream))
+                        {
+                            decryptedMessage = streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ EncryptionMessage/EncryptionMessage/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cryptographer Jon = new Cryptographer();
+            Cryptographer Bob = new Cryptographer();
+
+            Jon.CreateAesKeys("password1", "salt1");
+            Jon.CreateRsaKeys();
+
+            Bob.CreateAesKeys("password2", "salt2");
+            Bob.CreateRsaKeys();
+
+            Jon.publicKeyRsaIn = Bob.publicKeyRsaOut;
+            Bob.publicKeyRsaIn = Jon.publicKeyRsaOut;
+
+            Jon.EncryptRsa(Jon.symmetricKeyOut);
+            Bob.DecryptRsa(Jon.encryptedData);
+            Bob.symmetricKeyIn = Bob.decryptedData;
+
+            Jon.EncryptRsa(Jon.symmetricIVOut);
+            Bob.DecryptRsa(Jon.encryptedData);
+            Bob.symmetricIVIn = Bob.decryptedData;
+
+            Bob.EncryptRsa(Bob.symmetricKeyOut);
+            Jon.DecryptRsa(Bob.encryptedData);
+            Jon.symmetricKeyIn = Jon.decryptedData;
+
+            Bob.EncryptRsa(Bob.symmetricIVOut);
+            Jon.DecryptRsa(Bob.encryptedData);
+            Jon.symmetricIVIn = Jon.decryptedData;
+
+            Bob.EncryptAes("Hello, Jon!");
+            Jon.DecryptAes(Bob.encryptedMessage);
+            Console.WriteLine($"Jon received message: {Jon.decryptedMessage}");
+
+            Jon.EncryptAes("Hello, Bob!");
+            Bob.DecryptAes(Jon.encryptedMessage);
+            Console.WriteLine($"Bob received message: {Bob.decryptedMessage}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Bob encrypted message: {Convert.ToBase64String(Bob.encryptedMessage)}");
+            Console.WriteLine($"Jon encrypted message: {Convert.ToBase64String(Jon.encryptedMessage)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Jon transmitted symmetric key: {Convert.ToBase64String(Jon.symmetricKeyOut)}");
+            Console.WriteLine($"Jon received symmetric key: {Convert.ToBase64String(Jon.symmetricKeyIn)}");
+            Console.WriteLine($"Bob transmitted symmetric key: {Convert.ToBase64String(Bob.symmetricKeyOut)}");
+            Console.WriteLine($"Bob received symmetric key: {Convert.ToBase64String(Bob.symmetricKeyIn)}");
+
+            Console.WriteLine($"\nJon public RSA modulus: {Convert.ToBase64String(Jon.publicKeyRsaOut.Modulus)}");
+            Console.WriteLine($"\nBob public RSA modulus: {Convert.ToBase64String(Bob.publicKeyRsaOut.Modulus)}");
+        }
+    }
+}
